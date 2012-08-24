de066c4b02e7a8c5181bcef35849aa18abd36334
Elizabeth Flanagan <elizabeth.flanagan@intel.com>
2012-08-24 15:35:16 -0700

    yoctoABConfig: LICENSE_FLAGS_WHITELIST append
    LICENSE_FLAGS_WHITELIST should be appended to.
    
    Signed-off-by: Elizabeth Flanagan <elizabeth.flanagan@intel.com>

--- yocto-master/yoctoABConfig.py
+++ yocto-master/yoctoABConfig.py
@@ -308,7 +308,7 @@ def createAutoConf(factory, defaultenv, btarget=None, distro=None, buildhistory=
     fout = fout + 'SDKMACHINE ?= "i586"\n'
     fout = fout + 'DL_DIR = "' + defaultenv['DL_DIR']+'"\n'
     if str(btarget) == "fri2" or str(btarget) == "crownbay" or "sys940x":
-        fout = fout + 'LICENSE_FLAGS_WHITELIST = "license_emgd-driver-bin" \n'
+        fout = fout + 'LICENSE_FLAGS_WHITELIST += "license_emgd-driver-bin" \n'
     if str(btarget) == "cedartrail":
         fout = fout + 'LICENSE_FLAGS_WHITELIST += "license_cdv-pvr-driver" \n'
         fout = fout + 'PVR_LICENSE = "yes" \n'
