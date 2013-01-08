f6d3094a4ff1e6925a5d973e0e315d2df04d29cd
Elizabeth Flanagan <elizabeth.flanagan@intel.com>
2013-01-08 10:48:19 -0800

    yoctoAB: Adding more build steps.
    We're close to a working configuration. Create AutoConf doesn't quite function
    yet.
    
    Signed-off-by: Elizabeth Flanagan <elizabeth.flanagan@intel.com>

--- yocto-master/yoctoABConfig.py
+++ yocto-master/yoctoABConfig.py
@@ -1,7 +1,13 @@
 '''
-Created on Dec 4, 2012
+Created on Dec 29, 2012
 
-@author: pidge
+__author__ = "Elizabeth 'pidge' Flanagan"
+__copyright__ = "Copyright 2012-2013, Intel Corp."
+__credits__ = ["Elizabeth Flanagan"]
+__license__ = "GPL"
+__version__ = "2.0"
+__maintainer__ = "Elizabeth Flanagan"
+__email__ = "elizabeth.flanagan@intel.com"
 '''
 
 if __name__ == '__main__':
@@ -11,5 +17,4 @@ if __name__ == '__main__':
     yocto_buildsets = Autobuilder()
     yocto_buildsets.parseConfig()
     yocto_buildsets.createBuildsets()
-    
-    
+
